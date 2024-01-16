bbe04a7e4ae46f73130d8c9e56461e8642b1caf4
Joe Tannenbaum <joe@joe.codes>
2024-01-15 19:48:34 -0500

    mobile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon"
-        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧪</text></svg>">
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚗️</text></svg>">
 
     <title>CLI Lab by Joe Tannenbaum</title>
 
@@ -55,7 +55,7 @@
 <body class="antialiased bg-slate-950 text-white font-mono">
     <div class="h-screen flex flex-col justify-center items-center">
         <div class="text-center">
-            <pre class="inline-block text-sky-300">
+            <pre class="inline-block text-sky-300 text-xs md:text-base">
  ______  __      __       __      ______  ______
 /\  ___\/\ \    /\ \     /\ \    /\  __ \/\  == \
 \ \ \___\ \ \___\ \ \    \ \ \___\ \  __ \ \  __<
@@ -69,10 +69,11 @@
         </div>
 
         <div x-data="{ input: '{{ \App\Lab\Support\SSH::command() }}' }">
-            <div class="text-3xl mt-12 relative cursor-pointer hover:text-sky-300 transition-colors" x-clipboard="input"
-                title="Copy to Clipboard">
+            <div class="text-base md:text-3xl mt-12 relative cursor-pointer hover:text-sky-300 transition-colors"
+                x-clipboard="input" title="Copy to Clipboard">
                 <span>{{ \App\Lab\Support\SSH::command() }}</span>
-                <span class="w-1.5 h-8 inline-block animate-blink bg-sky-300 absolute left-full top-1 ml-3"></span>
+                <span
+                    class="w-1 md:w-1.5 h-4 md:h-8 inline-block animate-blink bg-sky-300 absolute left-full top-1 ml-2 md:ml-3"></span>
             </div>
         </div>
     </div>
